633f227a75c3a79fa4383c6b042f103d266b60d6
Rohith Kumar Gujarathi <118886530+RohithReacts@users.noreply.github.com>
2025-11-13 21:14:46 +0530

    push

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,5 @@
 import { LoginForm } from "@/components/forms/login-form";
+import { ModeToggle } from "@/components/templates/darkmode";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -10,15 +11,19 @@ export default function LoginPage() {
   return (
     <div className="flex items-center justify-center">
       <div className="w-full max-w-sm px-4">
+        <div
+          className="
+    fixed 
+    top-4 right-4         
+    sm:top-6 sm:right-8   
+    md:top-6 md:right-12  
+    lg:top-8 lg:right-20  
+  "
+        >
+          <ModeToggle />
+        </div>
         <LoginForm />
       </div>
     </div>
-
   );
 }
-
-
-
-
-   
-
